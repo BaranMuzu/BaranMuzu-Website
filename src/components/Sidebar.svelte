475e13ca97dd3e772409d8cfc26e1460b9bcf9dc
Daveberry <84567826+daveberrys@users.noreply.github.com>
2026-04-11 14:46:47 +0800

    sidebar wiring

--- src/components/Sidebar.svelte
+++ src/components/Sidebar.svelte
@@ -1,3 +1,29 @@
+<script>
+    export let currentPath = "/";
+
+    class SidebarLink {
+        constructor(label, href) {
+            this.label = label;
+            this.href = href;
+        }
+    }
+
+    let links = [
+        new SidebarLink("Home", "/"),
+        new SidebarLink("Fan Arts", "/fanarts"),
+        new SidebarLink("Credits", "/credits"),
+        new SidebarLink("Projects", "/projects")
+    ];
+
+    function checkActiveLink(href) {
+        if (currentPath == href) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+</script>
+
 <div class="main">
     <div class="top">
         <img
@@ -9,12 +35,11 @@
         <span class="mainText">I'm Baran!</span>
         <span>Welcome to my page, looks like you want to learn more about me. So here we go!</span>
     </div>
-    
+
     <div class="bottom">
-        <a class="active" href="/">Home</a>
-        <a class="" href="/fanarts">Fan Arts</a>
-        <a class="" href="/credits">Credits</a>
-        <a class="" href="/projects">Projects</a>
+        {#each links as link}
+            <a class:active={checkActiveLink(link.href)} href={link.href}>{link.label}</a>
+        {/each}
     </div>
 </div>
 
@@ -23,21 +48,21 @@
         display: flex;
         flex-direction: column;
         align-items: center;
-        
+
         min-width: 15rem;
         max-width: 15rem;
         height: 100vh;
         padding: 10px;
-        
+
         border-right: 2px solid var(--sidebarRightBorder);
-        
+
         .top {
             margin-top: 2rem;
             display: flex;
             flex-direction: column;
             align-items: center;
             gap: 5px;
-            
+
             .mainText {
                 color: var(--primary);
                 font-size: 1.5rem;
@@ -50,15 +75,15 @@
             flex-direction: column;
             gap: 5px;
             margin-top: 5rem;
-            
+
             a {
                 background-color: var(--secondary);
                 color: var(--secondaryText);
-                
+
                 font-weight: bold;
                 text-align: center;
                 text-decoration: none;
-                
+
                 padding: 0.5rem 1rem;
                 width: 10rem;
                 border-radius: 5px;
