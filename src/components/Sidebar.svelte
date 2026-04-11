272fe1737f678db5c314c958d9ae4ce3cc8efe13
Daveberry <84567826+daveberrys@users.noreply.github.com>
2026-04-11 13:56:15 +0800

    a scaffolded file structure

---
+++ src/components/Sidebar.svelte
@@ -0,0 +1,32 @@
+<div class="main">
+    <div class="top">
+        
+    </div>
+    
+    <div class="bottom">
+        <a href="/">Home</a>
+        <a href="/">Fan Arts</a>
+        <a href="/">About Me</a>
+        <a href="/">Projects</a>
+    </div>
+</div>
+
+<style>
+    .main {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        
+        .top {
+            display: flex;
+            flex-direction: column;
+            gap: 5px;
+        }
+
+        .bottom {
+            display: flex;
+            flex-direction: column;
+            gap: 5px;
+        }
+    }
+</style>
