b748f115aeddc50c1df6b03cdef4219f930fb132
Daveberry <84567826+daveberrys@users.noreply.github.com>
2026-04-11 14:19:18 +0800

    assets + sidebar (oh god so many file AHH-)

---
+++ public/README.md
@@ -0,0 +1,5 @@
+Howdy!
+
+The reason why it's `/public/assets/` is because of file organization. Okay, bye.
+
+Check me out at https://codedave.pages.dev/ :3
--- src/components/Sidebar.svelte
+++ src/components/Sidebar.svelte
@@ -1,13 +1,20 @@
 <div class="main">
     <div class="top">
-        
+        <img
+            src="/assets/images/spin.gif"
+            width="128"
+            height="128"
+            alt="baran spin!!"
+        />
+        <span class="mainText">I'm Baran!</span>
+        <span>Welcome to my page, looks like you want to learn more about me. So here we go!</span>
     </div>
     
     <div class="bottom">
-        <a href="/">Home</a>
-        <a href="/">Fan Arts</a>
-        <a href="/">About Me</a>
-        <a href="/">Projects</a>
+        <a class="active" href="/">Home</a>
+        <a class="" href="/fanarts">Fan Arts</a>
+        <a class="" href="/credits">Credits</a>
+        <a class="" href="/projects">Projects</a>
     </div>
 </div>
 
@@ -15,18 +22,50 @@
     .main {
         display: flex;
         flex-direction: column;
-        justify-content: space-between;
+        align-items: center;
+        
+        min-width: 15rem;
+        max-width: 15rem;
+        height: 100vh;
+        padding: 10px;
+        
+        border-right: 2px solid var(--sidebarRightBorder);
         
         .top {
+            margin-top: 2rem;
             display: flex;
             flex-direction: column;
+            align-items: center;
             gap: 5px;
+            
+            .mainText {
+                color: var(--primary);
+                font-size: 1.5rem;
+                font-weight: bold;
+            }
         }
 
         .bottom {
             display: flex;
             flex-direction: column;
             gap: 5px;
+            margin-top: 5rem;
+            
+            a {
+                background-color: var(--secondary);
+                color: var(--secondaryText);
+                
+                font-weight: bold;
+                text-align: center;
+                text-decoration: none;
+                
+                padding: 0.5rem 1rem;
+                width: 10rem;
+                border-radius: 5px;
+            } a.active {
+                background-color: var(--primary);
+                border-color: var(--border);
+            }
         }
     }
 </style>
